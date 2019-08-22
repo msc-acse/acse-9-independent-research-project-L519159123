958bc6b5f27860d95c1a7bccfcec0f305bfc9008
519159123 <13611006229@163.com>
2019-08-22 17:14:04 +0100

    first commit with small folder

---
+++ data/aligned_dataset.py
@@ -0,0 +1,69 @@
+import os.path
+import torch
+import numpy as np
+from data.base_dataset import BaseDataset, get_params, get_transform
+from data.image_folder import make_dataset
+from PIL import Image
+import random
+
+def randomCrop(data1, data2, width, height):
+    assert width <= data1.shape[0]
+    assert height <= data1.shape[1]
+    crop_w = random.randint(0,data1.shape[0] - width)
+
+    crop_h = random.randint(0,data1.shape[1] - height)
+
+    data1 = data1[crop_w:crop_w+width, crop_h:crop_h+height]
+    data2 = data2[crop_w:crop_w+width, crop_h:crop_h+height]
+    return data1, data2
+
+class AlignedDataset(BaseDataset):
+    """A dataset class for paired image dataset.
+
+    It assumes that the directory '/path/to/data/train' contains image pairs in the form of {A,B}.
+    During test time, you need to prepare a directory '/path/to/data/test'.
+    """
+
+    def __init__(self, opt):
+        """Initialize this dataset class.
+
+        Parameters: 
+            opt (Option class) -- stores all the experiment flags; needs to be a subclass of BaseOptions
+        """
+        BaseDataset.__init__(self, opt)
+        self.dir_AB = os.path.join(opt.dataroot, opt.phase)  # get the image directory
+        self.AB_paths = sorted(make_dataset(self.dir_AB, opt.max_dataset_size))  # get image paths
+        assert(self.opt.load_size >= self.opt.crop_size)   # crop_size should be smaller than the size of loaded image
+        self.input_nc = self.opt.output_nc if self.opt.direction == 'BtoA' else self.opt.input_nc
+        self.output_nc = self.opt.input_nc if self.opt.direction == 'BtoA' else self.opt.output_nc
+
+    def __getitem__(self, index):
+        """Return a data point and its metadata information.
+
+        Parameters:
+            index - - a random integer for data indexing
+
+        Returns a dictionary that contains A, B, A_paths and B_paths
+            A (tensor) - - an image in the input domain
+            B (tensor) - - its corresponding image in the target domain
+            A_paths (str) - - image paths
+            B_paths (str) - - image paths (same as A_paths)
+        """
+        # read a image given a random integer index
+        AB_path = self.AB_paths[index]
+        AB = np.genfromtxt(AB_path, delimiter=',', dtype='float64').astype('float32')
+        # split the paired data
+        A = AB[:, :240]
+        B = AB[:, 240:]
+        # convert array to tensor
+        tensor_A = torch.tensor(A, dtype = torch.float)
+        tensor_B = torch.tensor(B, dtype = torch.float)
+        # add one channel
+        A = tensor_A.unsqueeze(0)
+        B = tensor_B.unsqueeze(0)
+
+        return {'A': A, 'B': B, 'A_paths': AB_path, 'B_paths': AB_path}
+
+    def __len__(self):
+        """Return the total number of images in the dataset."""
+        return len(self.AB_paths)
---
+++ data/image_folder.py
@@ -0,0 +1,117 @@
+"""A modified image folder class
+
+We modify the official PyTorch image folder (https://github.com/pytorch/vision/blob/master/torchvision/datasets/folder.py)
+so that this class can load images from both current directory and its subdirectories.
+"""
+
+import torch.utils.data as data
+
+from PIL import Image
+import os
+import os.path
+
+
+# IMG_EXTENSIONS = [
+#     '.jpg', '.JPG', '.jpeg', '.JPEG',
+#     '.png', '.PNG', '.ppm', '.PPM', '.bmp', '.BMP',
+# ]
+
+data_suffix = ['.txt']
+
+# def is_image_file(filename):
+#     return any(filename.endswith(extension) for extension in IMG_EXTENSIONS)
+
+def is_data_file(filename):
+    return any(filename.endswith(extension) for extension in data_suffix)
+
+# def make_dataset(dir, max_dataset_size=float("inf")):
+#     images = []
+#     assert os.path.isdir(dir), '%s is not a valid directory' % dir
+
+#     for root, _, fnames in sorted(os.walk(dir)):
+#         for fname in fnames:
+#             if is_image_file(fname):
+#                 path = os.path.join(root, fname)
+#                 images.append(path)
+#     return images[:min(max_dataset_size, len(images))]
+
+
+def make_dataset(dir, max_dataset_size=float("inf")):
+    data = []
+    assert os.path.isdir(dir), '%s is not a valid directory' % dir
+
+    for root, _, fnames in sorted(os.walk(dir)):
+        for fname in fnames:
+            if is_data_file(fname):
+                path = os.path.join(root, fname)
+                data.append(path)
+    return data[:min(max_dataset_size, len(data))]
+
+
+
+# def default_loader(path):
+#     return Image.open(path).convert('RGB')
+
+def default_loader(path):
+    return np.genfromtxt(directory, delimiter=',', dtype='float64').astype('float32')
+
+
+# class ImageFolder(data.Dataset):
+
+#     def __init__(self, root, transform=None, return_paths=False,
+#                  loader=default_loader):
+#         imgs = make_dataset(root)
+#         if len(imgs) == 0:
+#             raise(RuntimeError("Found 0 images in: " + root + "\n"
+#                                "Supported image extensions are: " +
+#                                ",".join(IMG_EXTENSIONS)))
+
+#         self.root = root
+#         self.imgs = imgs
+#         self.transform = transform
+#         self.return_paths = return_paths
+#         self.loader = loader
+
+#     def __getitem__(self, index):
+#         path = self.imgs[index]
+#         img = self.loader(path)
+#         if self.transform is not None:
+#             img = self.transform(img)
+#         if self.return_paths:
+#             return img, path
+#         else:
+#             return img
+
+#     def __len__(self):
+#         return len(self.imgs)
+    
+    
+
+class ImageFolder(data.Dataset):
+
+    def __init__(self, root, transform=None, return_paths=False,
+                 loader=default_loader):
+        data = make_dataset(root)
+        if len(data) == 0:
+            raise(RuntimeError("Found 0 text file in: " + root + "\n"
+                               "Supported data suffix are: " +
+                               ",".join(data_suffix)))
+
+        self.root = root
+        self.data = data
+        self.transform = transform
+        self.return_paths = return_paths
+        self.loader = loader
+
+    def __getitem__(self, index):
+        path = self.data[index]
+        da = self.loader(path)
+        if self.transform is not None:
+            da = self.transform(da)
+        if self.return_paths:
+            return da, path
+        else:
+            return da
+
+    def __len__(self):
+        return len(self.data)
