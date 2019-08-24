a45faeaebfabab56aad6078c8cd6c0ecb423963b
519159123 <13611006229@163.com>
2019-08-24 17:40:29 +0100

    add download_data file

---
+++ download_data.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+
+echo 'Downloading the data, trained model and results'
+wget http://data-liu.yaospace.com/liu_project_data.tar.gz
+tar xvzf liu_project_data.tar.gz
+rm liu_project_data.tar.gz
