2bf5d226b16d7e69acce6122f1b84d0378761576
Yuxuan Liu <43777719+L519159123@users.noreply.github.com>
2019-08-24 18:04:35 +0100

    Update train_cyclegan.sh

--- scripts/train_cyclegan.sh
+++ scripts/train_cyclegan.sh
@@ -1,2 +1,2 @@
 set -ex
-python train.py --dataroot ./datasets/maps --name maps_cyclegan --model cycle_gan --pool_size 50 --no_dropout
+python train.py --dataroot ./datasets//seismic/PZ_combination_crop_900 --name seismic_cyclegan --model cycle_gan --pool_size 50 --no_dropout
