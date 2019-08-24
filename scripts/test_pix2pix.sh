2c47302526de01a401bd648bea4d9b27e6576ee3
Yuxuan Liu <43777719+L519159123@users.noreply.github.com>
2019-08-24 18:06:58 +0100

    Update test_pix2pix.sh

--- scripts/test_pix2pix.sh
+++ scripts/test_pix2pix.sh
@@ -1,2 +1,2 @@
 set -ex
-python test.py --dataroot ./datasets/facades --name facades_pix2pix --model pix2pix --netG unet_256 --direction BtoA --dataset_mode aligned --norm batch
+python test.py --dataroot ./datasets/seismic/PZ_combination_crop_900 --name seismic_pix2pix --model pix2pix --netG unet_256 --direction AtoB --dataset_mode aligned --norm batch
