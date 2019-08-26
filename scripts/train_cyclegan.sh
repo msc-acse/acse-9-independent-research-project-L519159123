c46e848bff42fb0f786e4edd37b7f54b4f7c3bc9
519159123 <13611006229@163.com>
2019-08-26 12:25:41 +0100

    delete useless scripts

--- scripts/train_cyclegan.sh
+++
@@ -1,2 +0,0 @@
-set -ex
-python train.py --dataroot ./datasets/seismic/PZ_combination_crop_900 --name seismic_cyclegan --model cycle_gan --pool_size 50 --no_dropout
--- scripts/train_pix2pix.sh
+++
@@ -1,2 +0,0 @@
-set -ex
-python train.py --dataroot ./datasets/seismic/PZ_combination_crop_900 --name seismic_pix2pix --model pix2pix --netG unet_256 --direction AtoB --lambda_L1 100 --dataset_mode aligned --norm batch --pool_size 0
