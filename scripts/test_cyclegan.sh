f8b2d800b26efd1057a7edbe2ab7d8167c60d5b0
519159123 <13611006229@163.com>
2019-08-26 12:43:17 +0100

    modify the test script

--- scripts/test_cyclegan.sh
+++ scripts/test_cyclegan.sh
@@ -1,2 +1,2 @@
 set -ex
-python test.py --dataroot ./datasets/seismic/PZ_combination_crop_900 --name seismic_cyclegan --model cycle_gan --phase test --no_dropout
+python test.py --dataroot ./datasets/seismic/PZ_combination_crop_900 --name seismic_cyclegan --model cycle_gan --direction AtoB --input_nc 1 --output_nc 1 --netG resnet_9blocks --ngf 64 --ndf 8 --n_layers_D 1 --dataset_mode aligned --netD pixel
--- scripts/test_pix2pix.sh
+++ scripts/test_pix2pix.sh
@@ -1,2 +1,2 @@
 set -ex
-python test.py --dataroot ./datasets/seismic/PZ_combination_crop_900 --name seismic_pix2pix --model pix2pix --netG unet_256 --direction AtoB --dataset_mode aligned --norm batch
+python test.py --dataroot ./datasets/seismic/PZ_combination_crop_900 --name seismic_pix2pix --model pix2pix --netG resnet_9blocks --netD pixel --ngf 64 --ndf 16 --direction AtoB --n_layers_D 1 --input_nc 1 --output_nc 1
